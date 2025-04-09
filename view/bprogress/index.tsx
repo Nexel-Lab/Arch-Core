d4b664985220ebf5ad8a094f5cabe754fb5552e1
Jirayu-ninl <Jirayu.ninl@gmail.com>
2025-04-09 20:26:51 +0700

    [feat] client: add view/bprogress

---
+++ view/bprogress/index.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { AppProgressProvider as BProgress } from '@bprogress/next'
+import { useUiState } from '@/store'
+
+export const Progress = ({
+  color,
+}: {
+  color: { light: string; dark: string }
+}) => {
+  const _dark = useUiState((st) => st.dark)
+  return (
+    <BProgress
+      color={_dark ? color.dark : color.light}
+      options={{
+        showSpinner: false,
+        parent: '#nprogress-nav-parent',
+      }}
+      shallowRouting
+    />
+  )
+}
